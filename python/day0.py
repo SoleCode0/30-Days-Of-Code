c526460ed5badf6edd64b5bb0e6b22a11f2e66ec
Mohammad Danish <mohddanish8299@gmail.com>
2022-01-27 12:17:53 +0530

    updated day0 theory in py

--- python/day0.py
+++ python/day0.py
@@ -1,3 +1,29 @@
+# Task
+'''
+To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. on a single line, and finally print the value of your variable on a second line.
+
+You've got this!
+
+Note: The instructions are Java-based, but we support submissions in many popular languages. You can switch languages using the drop-down menu above your editor, and the inputString variable may be written differently depending on the best-practice conventions of your submission language.
+'''
+
+# Sample Input
+'''
+A single line of text denoting inputString (the variable whose contents must be printed).
+
+Welcome to 30 Days of Code!
+'''
+
+# Sample Output
+'''
+Print Hello, World. on the first line, and the contents of inputString on the second line.
+
+Hello, World. 
+Welcome to 30 Days of Code!
+'''
+
+# Code
+
 # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
 # input something here
 input_string = input()
